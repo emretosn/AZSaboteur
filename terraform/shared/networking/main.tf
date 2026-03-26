4abe7477541b8367bd0b136c049987b01c482c85
Emre Tosun <emre7osun@gmail.com>
2026-03-26 11:03:05 +0100

    nsg priority

--- terraform/shared/networking/main.tf
+++ terraform/shared/networking/main.tf
@@ -57,7 +57,7 @@ resource "azurerm_network_security_group" "kali" {
 
 resource "azurerm_network_security_rule" "kali_allow_rdp" {
   name                        = "AllowRDP"
-  priority                    = 100
+  priority                    = 200
   direction                   = "Inbound"
   access                      = "Allow"
   protocol                    = "Tcp"
